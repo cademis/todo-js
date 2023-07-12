10921fd967177301618ced8f63c5e50a1e67b6e3
Chris <caitken86@gmail.com>
2023-07-12 10:07:05 +0100

    add area checkboxes to sidebar

--- src/index.js
+++ src/index.js
@@ -17,98 +17,116 @@ export const renderHeader = () => {
   const headerElement = createElement("header", containerElement);
   const createArea = createElement("button", headerElement);
   createArea.type = "button";
-  createArea.innerHTML = "Create Area";
+  createArea.innerHTML = "Add Area";
   const createProject = createElement("button", headerElement);
   createProject.type = "button";
-  createProject.innerHTML = "Create Project";
+  createProject.innerHTML = "Add Project";
 };
 
 export const renderSidebar = () => {
   const sidebarElement = createElement("div", containerElement, "sidebar");
-  const input = createElement("input", sidebarElement);
-  input.placeholder = "Search";
+  const input = createElement("input", sidebarElement, "filter-area");
+  input.placeholder = "Filter Area";
+  const div = createElement("div", sidebarElement, "checkboxes");
+  const ul = createElement("ul", div);
+  let areas = [
+    { areaId: "H6", description: "Health and Fitness" },
+    { areaId: "P2", description: "Politics" },
+    { areaId: "H9", description: "Holiday Planning" },
+  ];
+  areas.forEach((area) => {
+    const li = createElement("li", ul);
+    const input = createElement("input", li);
+    input.type = "checkbox";
+    const label = createElement("label", li);
+    label.textContent = area.description;
+  });
 };
 
 export const renderMain = () => {
   const mainElement = createElement("main", containerElement);
+  const input = createElement("input", mainElement, "filter-project");
+  input.placeholder = "Filter Project";
+
   const table = createElement("table", mainElement);
   const thead = createElement("thead", table);
   const tr = createElement("tr", thead);
-  const tableHeadings = ["Project ID", "Description", "Folder Link"];
+  const tableHeadings = ["Project ID", "Description", "Created"];
   tableHeadings.forEach((item) => {
     const td = createElement("td", tr);
     td.innerHTML = item;
   });
   const tbody = createElement("tbody", table);
 
-  let data = [
+  let projects = [
     {
       projectId: "H6-001",
       description: "this is the first project",
-      folderLink: "H6/002",
+      created: "202307120925",
     },
     {
       projectId: "H6-002",
       description: "this is the second project",
-      folderLink: "H6/002",
+      created: "202307120926",
     },
     {
       projectId: "P2-001",
       description: "this is another random project",
-      folderLink: "P2/001",
+      created: "202307120927",
     },
     {
       projectId: "H9-001",
       description: "this is the third project",
-      folderLink: "H9/001",
+      created: "202307120928",
     },
     {
       projectId: "P5-001",
       description: "this is a new project",
-      folderLink: "P5/001",
+      created: "202307120929",
     },
     {
       projectId: "H8-001",
       description: "this is a test project",
-      folderLink: "H8/001",
+      created: "202307120930",
     },
     {
       projectId: "P3-001",
       description: "this is a sample project",
-      folderLink: "P3/001",
+      created: "202307120931",
     },
     {
       projectId: "H7-001",
       description: "this is an experimental project",
-      folderLink: "H7/001",
+      created: "202307120932",
     },
     {
       projectId: "P4-001",
       description: "this is a demo project",
-      folderLink: "P4/001",
+      created: "202307120933",
     },
     {
       projectId: "H10-001",
       description: "this is a new addition project",
-      folderLink: "H10/001",
+      created: "202307120934",
     },
     {
       projectId: "P6-001",
       description: "this is a development project",
-      folderLink: "P6/001",
+      created: "202307120935",
     },
   ];
-  data.forEach((item) => {
+
+  projects.forEach((project) => {
     const tr = createElement("tr", tbody);
     const projectId = createElement("td", tr);
-    projectId.innerHTML = item.projectId;
+    projectId.innerHTML = project.projectId;
     const description = createElement("td", tr);
-    description.innerHTML = item.description;
-    const folderLink = createElement("td", tr);
-    folderLink.innerHTML = item.folderLink;
+    description.innerHTML = project.description;
+    const created = createElement("td", tr);
+    created.innerHTML = project.created;
     tr.appendChild(projectId);
     tr.appendChild(description);
-    tr.appendChild(folderLink);
+    tr.appendChild(created);
   });
 };
 
