1da7cf1444a02172ca15fae521be5da8663ab75b
Chris <caitken86@gmail.com>
2023-07-12 05:23:00 +0100

    import date-fns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello todo js")
+import { format } from "date-fns";
+
+const result = format(new Date(), 'yyyyddMMHHmm')
+
+console.log(result)
+
+localStorage.setItem("1", "2")
+
+const first = localStorage.getItem("1")
+
+console.log(first)
